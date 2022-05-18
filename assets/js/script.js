77d90960ac117697a393bf02662433b8a46eeb20
Douglas Silva <silvatec.br@gmail.com>
2022-05-18 19:30:09 -0300

    Iniciando JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const inputValue = document.querySelector('#inputValue');
+const btnValue = document.querySelector('#btnValue');
+const imgQrCode = document.querySelector('#imgQrCode');
+const wrapper = document.querySelector('.wrapper');
+let valueDefault;
+
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
